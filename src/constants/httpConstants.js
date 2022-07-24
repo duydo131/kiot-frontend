3abf754522546b9912d2c04fcf814a196c808bc7
cuong1607 <dovancuong160700@gmail.com>
2022-07-24 15:32:18 +0700

    design profile user

--- src/constants/httpConstants.js
+++ src/constants/httpConstants.js
@@ -1,5 +1,5 @@
-export const REACT_APP_API_URL = "http://localhost:8000/api/v1"
-// export const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "http://192.168.6.105:8000/api/v1"
+// export const REACT_APP_API_URL = "http://localhost:8000/api/v1"
+export const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "http://192.168.1.6:8000/api/v1"
 
 
 export const DEFAULT_IMAGE_PRODUCT = "/static/default.jpeg"
